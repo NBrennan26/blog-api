d6f31625c45e2cd04ff40ebe66070445b7c4d73b
nbrennan26 <nbrennan26@gmail.com>
2022-04-14 10:47:33 -0500

    db config/mongoose connection added. errormiddleware/handler added. Basic server setup created

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import "dotenv/config"
+
+import routes from "./routes"
+import errorHandler from ".middleware/errorMiddleware"
+import connectDB from "/config/db"
+
+connectDB()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use("/api/posts", routes.posts)
+app.use("/api/users", routes.user)
+
+// Serve Frontend
+// COMING SOON! //
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`Server Started on port ${PORT}`)
+})
