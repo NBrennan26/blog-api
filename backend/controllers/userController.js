46594b361721e1904491acd9a6fd95776d5bcb85
nbrennan26 <nbrennan26@gmail.com>
2022-04-18 13:24:09 -0500

    Add logic to log user in (authenticated) to userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,11 +61,27 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Log In New User
+// @desc    Authenticate User
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Log In User" });
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+      admin: user.admin,
+      token: generateToken(user_id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Credentials");
+  }
 });
 
 // Generate JWT
