8627a68de75f85d9c0715769ae3303f31ad6ed70
nbrennan26 <nbrennan26@gmail.com>
2022-04-19 14:57:37 -0500

    Create initial components/pages. Install react-router-dom. Update App.js. Delete frontend README initial info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 }
 
