4be709bac376767f540706eca36591f4e0875aa3
nbrennan26 <nbrennan26@gmail.com>
2022-04-19 15:07:54 -0500

    create feature folders for auth, comments, and posts. install axios in frontend

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import commentReducer from "../features/comments/commentSlice";
+import postReducer from "../features/posts/postSlice";
 
 export const store = configureStore({
   reducer: {
-
+    auth: authReducer,
+    comments: commentReducer,
+    posts: postReducer,
   },
 });
