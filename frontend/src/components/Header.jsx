ac9856a0bda77828c2cfaf972accf6d1cecc05a0
nbrennan26 <nbrennan26@gmail.com>
2022-04-19 15:42:34 -0500

    Add logic to authSlice/Service, build out basic Header (sans style)

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,41 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
+
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div>
+        <Link to="/">Blog</Link>
+      </div>
+      <ul>
+        {user ? (
+          <li>
+            <button onClick={onLogout}>Log Out</button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Log In</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_URL = "api/users/";
+
+// Register User
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+// Log In User
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+// Log Out User
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const authService = {
+  register,
+  login,
+  logout,
+};
+
+export default authService;
