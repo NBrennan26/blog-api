d5becfcdffe686ffb7c485537895a04a1d15c5ed
nbrennan26 <nbrennan26@gmail.com>
2022-04-20 10:15:09 -0500

    Building Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,88 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login, reset } from "../features/auth/authSlice";
+
 function Login() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      // TOAST
+      //toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      username,
+      password,
+    };
+
+    dispatch(login(userData));
+  };
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <section>
+        <h1>Login</h1>
+      </section>
+      <section>
+        <form onSubmit={onSubmit}>
+          <div>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              placeholder="Enter your Username"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter Password"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
