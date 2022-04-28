74eafdf497b1e40b7a10368c18e57f709b97a654
nbrennan26 <nbrennan26@gmail.com>
2022-04-28 10:17:58 -0500

    CommentForm styling

--- frontend/src/components/CommentForm.jsx
+++ frontend/src/components/CommentForm.jsx
@@ -43,34 +43,37 @@ function CommentForm() {
   };
 
   return (
-    <>
-      <div>
-        {user && !formDisplay.isDisplayed ? (
-          <>
-            <button onClick={toggleFormDisplay}>Add Comment</button>
-          </>
-        ) : user && formDisplay.isDisplayed ? (
-          <>
-            <form onSubmit={onSubmit}>
-              <div>
-                <textarea
-                  id="text"
-                  name="text"
-                  value={text}
-                  placeholder="Provide Comment Content"
-                  onChange={(e) => setText(e.target.value)}
-                />
-              </div>
-              <div>
-                <button type="submit">Submit</button>
-              </div>
-            </form>
-          </>
-        ) : (
-          <></>
-        )}
-      </div>
-    </>
+    <div className="comment-form-cont">
+      {user && !formDisplay.isDisplayed ? (
+        <>
+          <button className="add-comment-btn" onClick={toggleFormDisplay}>
+            Add Comment
+          </button>
+        </>
+      ) : user && formDisplay.isDisplayed ? (
+        <>
+          <form className="comment-form" onSubmit={onSubmit}>
+            <div>
+              <textarea
+                id="text"
+                name="text"
+                className="comment-text"
+                value={text}
+                placeholder="Provide Comment Content"
+                onChange={(e) => setText(e.target.value)}
+              />
+            </div>
+            <div>
+              <button className="submit-comment-btn" type="submit">
+                Submit
+              </button>
+            </div>
+          </form>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
 
